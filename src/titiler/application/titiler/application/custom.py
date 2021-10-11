cd3a68030b6eb4bf1fd01a472e3f7e579470dc5b
Robin Wilson <robin@rtwilson.com>
2021-10-11 21:00:48 +0100

    Comment out difficult code

--- src/titiler/application/titiler/application/custom.py
+++ src/titiler/application/titiler/application/custom.py
@@ -43,27 +43,27 @@ ColorMapName = Enum(  # type: ignore
     "ColorMapName", [(a, a) for a in sorted(cmap.list())]
 )
 
-# CUSTOM TMS for EPSG:3413
-EPSG3413 = morecantile.TileMatrixSet.custom(
-    [-4194300, -4194300, 4194300, 4194300],
-    CRS.from_epsg(3413),
-    identifier="EPSG3413",
-    matrix_scale=[2, 2],
-)
+# # CUSTOM TMS for EPSG:3413
+# EPSG3413 = morecantile.TileMatrixSet.custom(
+#     [-4194300, -4194300, 4194300, 4194300],
+#     CRS.from_epsg(3413),
+#     identifier="EPSG3413",
+#     matrix_scale=[2, 2],
+# )
 
-# CUSTOM TMS for EPSG:6933
-# info from https://epsg.io/6933
-EPSG6933 = morecantile.TileMatrixSet.custom(
-    [-17357881.81713629, -7324184.56362408, 17357881.81713629, 7324184.56362408],
-    CRS.from_epsg(6933),
-    identifier="EPSG6933",
-    matrix_scale=[1, 1],
-)
-tms = tms.register([EPSG3413, EPSG6933])
+# # CUSTOM TMS for EPSG:6933
+# # info from https://epsg.io/6933
+# EPSG6933 = morecantile.TileMatrixSet.custom(
+#     [-17357881.81713629, -7324184.56362408, 17357881.81713629, 7324184.56362408],
+#     CRS.from_epsg(6933),
+#     identifier="EPSG6933",
+#     matrix_scale=[1, 1],
+# )
+# tms = tms.register([EPSG3413, EPSG6933])
 
-TileMatrixSetName = Enum(  # type: ignore
-    "TileMatrixSetName", [(a, a) for a in sorted(tms.list())]
-)
+# TileMatrixSetName = Enum(  # type: ignore
+#     "TileMatrixSetName", [(a, a) for a in sorted(tms.list())]
+# )
 
 
 def ColorMapParams(
